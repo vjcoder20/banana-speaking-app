aaee60665875266b29cd51a72f240e64188383d9
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-05-26 03:04:39 +0530

    ex-03

---
+++ app.js
@@ -0,0 +1,11 @@
+// var username = prompt("Give me username") -->input
+// var welcomemessage = "This script works" + username -->processing
+//alert(welcomemessage) -->output
+
+var buttontranslate = document.querySelector("#btn-translate")
+ 
+buttontranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked")
+})
+
+
