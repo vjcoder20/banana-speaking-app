db72f16140a4a22e29c9e2a6100a4740b1a3c37e
Vaibhav Jain <jain.vaibhav020@gmail.com>
2021-05-27 05:42:10 +0530

    ho gaya

--- app.js
+++ app.js
@@ -5,9 +5,23 @@
 var buttontranslate = document.querySelector("#btn-translate")
 var textinput = document.querySelector("#txt-input")
 var outputdiv = document.querySelector("#output");
-
+ var serverURL = "https://api.funtranslations.com/translate/minion.json";   //Ye minion ka API hai jo net se mila hai
+ function gettransationalURL(text){
+    return serverURL + "?" + "text=" + text;
+ }
+ function errorhandler(error){
+    console.log("error occured",error)
+    alert("Try after sometime error occured")
+ }
  function clickEventHandler (){
-     outputdiv.innerText = "askdksadaldaka " + textinput.value;
+     var Inputtext = textinput.value;  //Input
+     fetch(gettransationalURL(Inputtext))
+     .then(response => response.json())     //Processing done by server
+     .then(json => {
+         var translatedtext = json.contents.translated;
+         outputdiv.innerText = translatedtext;       //output
+     })
+    //  .catch(errorhandler)
  }
 buttontranslate.addEventListener("click", clickEventHandler)
 
